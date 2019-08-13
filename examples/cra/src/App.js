75ce81aad6bbaeb13b1b555861bf426336a665fe
SLO-MO-User <tuncel.halit@hotmail.com>
2019-08-13 20:04:08 +0300

    Add cdn, cra, ssr examples

---
+++ examples/cra/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  Ellipsis,
+  Ring,
+  DualRing,
+  Facebook,
+  Grid,
+  Hourglass,
+  Circle,
+  Heart,
+  Roller,
+  Spinner,
+  Ripple
+} from "react-css-spinners";
+
+function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#111",
+        textAlign: "center"
+      }}
+    >
+      <Ellipsis />
+      <Ring />
+      <DualRing />
+      <Facebook />
+      <Grid />
+      <Hourglass />
+      <Circle />
+      <Heart />
+      <Ripple />
+      <Roller />
+      <Spinner />
+    </div>
+  );
+}
+
+export default App;
